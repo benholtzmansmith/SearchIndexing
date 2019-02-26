139c4078f263fca6bf9abef3f94a3b568f619d59
benholtzmansmith <benjamin.holtzman.smith@gmail.com>
2019-02-26 09:38:10 -0500

    funspec

--- src/test/scala/SearchIndexing.scala
+++ src/test/scala/SearchIndexing.scala
@@ -1,44 +1,66 @@
-import org.scalatest.{ FunSuite, Matchers }
+import org.scalatest.{ FunSpec, Matchers }
 
 import scala.collection.mutable.{ Map => MM }
 
-class IndexSpec extends FunSuite with Matchers {
+class DictionarySpec extends FunSpec with Matchers {
   //Test Dictionary
-  test( "Dictionary empty dictionary gets new entry" ) {
+  describe( "An empty dictionary" ) {
     val emptyDicionary = Dictionary()
-
     val newDictionary = emptyDicionary.add( "ben" )
-    assert( newDictionary.index == MM( "b" -> List( "ben" ) ) )
+    it("should add a new entry"){
+      assert( newDictionary.index == MM( "b" -> List( "ben" ) ) )
+    }
   }
-  test( "Dictionary non empty dictionary word without a previous key gets added to the index" ) {
-    val dict = Dictionary( MM( "a" -> List( "alpha" ) ) )
+  describe( "A non-empty dictionary" ) {
+    describe("when word without a previous key"){
+      val dict = Dictionary( MM( "a" -> List( "alpha" ) ) )
+      describe("gets added to the index"){
+        val newDict = dict.add( "ben" )
+        it("should make a new entry"){
+          assert( newDict.index == MM( "b" -> List( "ben" ), "a" -> List( "alpha" ) ) )
+        }
+      }
+    }
+    describe("when word with a previous key"){
+      val dict = Dictionary( MM( "a" -> List( "alpha" ), "b" -> List( "ben" ) ) )
+      describe("gets added to the index"){
+        val newDict = dict.add( "adam" )
+        it("should make a new entry"){
+          assert( newDict.index == MM( "b" -> List( "ben" ), "a" -> List( "alpha", "adam" ) ) )
+        }
+      }
+    }
+  }
+}
 
-    val newDict = dict.add( "ben" )
-    assert( newDict.index == MM( "b" -> List( "ben" ), "a" -> List( "alpha" ) ) )
-  }
-  test( "Dictionary non empty dictionary word with a previous key gets added to the index" ) {
-    val dict = Dictionary( MM( "a" -> List( "alpha" ), "b" -> List( "ben" ) ) )
 
-    val newDict = dict.add( "adam" )
-    assert( newDict.index == MM( "b" -> List( "ben" ), "a" -> List( "alpha", "adam" ) ) )
-  }
-  //Test ThreeStringKeyDictionary
-  test( "ThreeStringKeyDictionary empty dictionary gets new entry" ) {
+class ThreeStringKeyDictionarySpec extends FunSpec with Matchers {
+
+  describe( "An empty dictionary" ) {
     val emptyDicionary = ThreeStringKeyDictionary()
-
     val newDictionary = emptyDicionary.add( "ben" )
-    assert( newDictionary.index == MM( List( "be" ) -> List( "ben" ) ) )
+    it("should add a new entry"){
+      assert( newDictionary.index == MM( List( "be" ) -> List( "ben" ) ) )
+    }
   }
-  test( "ThreeStringKeyDictionary [incomplete key] non empty dictionary word without a previous key gets added to the index" ) {
-    val dict = ThreeStringKeyDictionary( MM( List( "a" ) -> List( "alpha" ) ) )
-
-    val newDict = dict.add( "ben" )
-    assert( newDict.index == MM( List( "a", "be" ) -> List( "alpha", "ben" ) ) )
-  }
-  test( "ThreeStringKeyDictionary [complete key] non empty dictionary word without a previous key gets added to the index" ) {
-    val dict = ThreeStringKeyDictionary( MM( List( "a", "ab", "ac" ) -> List( "alpha" ) ) )
-
-    val newDict = dict.add( "ben" )
-    assert( newDict.index == MM( List( "a", "ab", "ac" ) -> List( "alpha" ), List( "be" ) -> List( "ben" ) ) )
+  describe( "A non-empty dictionary" ) {
+    describe("when word without a previous key"){
+      val dict = ThreeStringKeyDictionary( MM( List( "a" ) -> List( "alpha" ) ) )
+      describe("gets added to the index"){
+        val newDict = dict.add( "ben" )
+        it("should make a new entry"){
+          assert( newDict.index == MM( List( "a", "be" ) -> List( "alpha", "ben" ) ) )
+        }
+      }
+    }
+    describe("when word with a previous key"){
+      val dict = ThreeStringKeyDictionary( MM( List( "a", "ab", "ac" ) -> List( "alpha" ) ) )
+      describe("gets added to the index"){
+        val newDict = dict.add( "ben" )
+        it("should make a new entry"){
+          assert( newDict.index == MM( List( "a", "ab", "ac" ) -> List( "alpha" ), List( "be" ) -> List( "ben" ) ) )
+        }
+      }
+    }
   }
 }
